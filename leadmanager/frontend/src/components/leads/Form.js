d790f7979ba997f4cc028f9aad40ff725ad0d06f
Garrett Lovell <garrettlovell@Garretts-MacBook-Pro.local>
2019-04-03 19:14:53 -0400

    first commit

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  state = {};
+  render() {
+    return <h2>Form</h2>;
+  }
+}
+
+export default Form;
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Leads extends Component {
+  state = {};
+  render() {
+    return <h2>Leads List</h2>;
+  }
+}
+
+export default Leads;
